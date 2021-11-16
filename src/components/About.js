6639a317a861d12d31c1821d128f09f95dfe2d1e
pmpurcell93 <maddenpurcell@gmail.com>
2021-11-15 20:26:06 -0600

    Set up basic layout for Home/About Page

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,46 @@ export default function About() {
   return (
     <div>
       <Navigation />
-      <h1>About</h1>
+      <div id="aboutDiv">
+        <div id="images">
+          <div className="image-div">
+            <img src="" alt="Item 1" />
+            <h4>Header</h4>
+            <p>This is info text</p>
+          </div>
+          <div className="image-div">
+            <img src="" alt="Item 1" />
+            <h4>Header</h4>
+            <p>This is info text</p>
+          </div>
+          <div className="image-div">
+            <img src="" alt="Item 1" />
+            <h4>Header</h4>
+            <p>This is info text</p>
+          </div>
+          <div className="image-div">
+            <img src="" alt="Item 1" />
+            <h4>Header</h4>
+            <p>This is info text</p>
+          </div>
+        </div>
+        <div id="aboutText">
+          <h1>About</h1>
+          <p>
+            I am a lifelong Tennessee resident who enjoys music and teaching. I
+            graduated from Tennessee Technological University with a degree in
+            History and Spanish, and from there I entered the field of informal
+            education and museum learning.
+          </p>
+          <p>
+            Like many people, my life and career changed forever with the onset
+            of COVID-19. With my work hours reduced, I decided to pick up coding
+            in my free time, and immediately fell in love. I enrolled in
+            Nashville Software School and began my Web-Development Boot Camp in
+            July of 2021.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
