86aee20aebd784810d4f9b623b15280def064f9f
pmpurcell93 <maddenpurcell@gmail.com>
2021-11-20 12:48:50 -0600

    Contact form and about carousel

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,6 +4,45 @@ export default function Contact() {
   return (
     <div>
       <h1>Contact</h1>
+      <div id="contactCards">
+        <div className="contact-link">
+          <a
+            href="https://www.linkedin.com/in/madden-purcell/"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <img
+              src="https://cdn.worldvectorlogo.com/logos/linkedin.svg"
+              alt="LinkedIn"
+              style={{ width: '200px', height: '200px' }}
+            />
+          </a>
+        </div>
+        <div className="contact-link">
+          <a
+            href="https://www.linkedin.com/in/madden-purcell/"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <img
+              src="https://cdn.worldvectorlogo.com/logos/github-2.svg"
+              alt="Github"
+              style={{ width: '100px', height: '100px' }}
+            />
+          </a>
+        </div>
+        <div className="contact-link">
+          <img
+            src="https://cdn.worldvectorlogo.com/logos/gmail-icon.svg"
+            alt="Email Icon"
+            style={{ width: '100px', height: '100px' }}
+          />
+          <a href="mailto: maddenpurcell@gmail.com">
+            {' '}
+            maddenpurcell@gmail.com{' '}
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
