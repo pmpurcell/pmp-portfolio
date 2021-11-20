83ad45a30a27c687b49fc338b4c69f26b16a6af4
pmpurcell93 <maddenpurcell@gmail.com>
2021-11-19 20:05:24 -0600

    Admin auth completed

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,29 +1,14 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
 import { PropTypes } from 'prop-types';
-import About from '../components/About';
-import Contact from '../components/Contact';
-import Create from '../components/Create';
-import Edit from '../components/Edit';
-import ProjectDetails from '../components/ProjectDetails';
-import Projects from '../components/Projects';
-import Technologies from '../components/Technologies';
+import AdminRoutes from './AdminRoutes';
+import PublicRoutes from './PublicRoutes';
 
 export default function Routes({ user }) {
   return (
-    <Switch>
-      <Route exact path="/" component={About} />
-      <Route exact path="/projects" component={Projects} />
-      <Route exact path="/tech" component={Technologies} />
-      <Route exact path="/contact" component={Contact} />
-      <Route exact path="/create" component={Create} />
-      <Route
-        exact
-        path="/details/:firebaseKey"
-        component={() => <ProjectDetails user={user} />}
-      />
-      <Route exact path="/edit/:firebaseKey" component={Edit} />
-    </Switch>
+    <>
+      {user?.isAdmin && <AdminRoutes />}
+      <PublicRoutes user={user} />
+    </>
   );
 }
 
